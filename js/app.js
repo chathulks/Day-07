9cc51abe4bc8b100ec7ea3cbadc8a4c9aff1dd58
chathulks <sadeeshachathumal5@gmail.com>
2026-07-18 09:31:19 +0530

    Refactor guessingNumber function for improved readability and formatting

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
-function guessingNumber(){
+function guessingNumber() {
 
-    let randomNumber = Math.random()*10+1;
+    let randomNumber = Math.random() * 10 + 1;
     let num = Math.floor(randomNumber);
 
     let userNumber = document.getElementById("guessNumber").value;
 
-    if(userNumber == ""){
+    if (userNumber == "") {
         alert("Please enter your number.!");
-    }else if(num == userNumber){
+    } else if (num > userNumber) {
+        alert("Your enter number Greater than Random number!");
+    } else if (num < userNumber) {
+        alert("Your enter number Less than Random number!");
+    } else if (num == userNumber) {
         alert("Your Guessing Success.!");
-    }else{
+    } else {
         alert("Tryagin Leter.!");
     }
-    
+
 }
