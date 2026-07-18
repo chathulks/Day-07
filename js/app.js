9127e72a9da09e1294f84450b853b388ce3c16d4
chathulks <sadeeshachathumal5@gmail.com>
2026-07-18 09:57:44 +0530

    Add SweetAlert2 for user feedback in guessingNumber function

--- js/app.js
+++ js/app.js
@@ -6,6 +6,13 @@ function guessingNumber() {
     let userNumber = document.getElementById("guessNumber").value;
 
     if (userNumber == "") {
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 1500
+        });
         alert("Please enter your number.!");
     } else if (num > userNumber) {
         alert("Your enter number Greater than Random number!");
